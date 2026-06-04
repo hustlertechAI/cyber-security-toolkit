5169828eaa48d8d23abbcfc717cc125cffabaac3
Hustler Tech Academy <hustlertech86@gmail.com>
2026-06-04 23:15:49 +0300

    Create passwordGenerator.ts

---
+++ src/src/passwordGenerator.ts
@@ -0,0 +1,13 @@
+export function generatePassword(length: number = 16): string {
+  const characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
+
+  let password = "";
+
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.floor(Math.random() * characters.length);
+    password += characters[randomIndex];
+  }
+
+  return password;
+}
